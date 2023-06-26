43c85d076f950564b5a1c3269cd95ef87cd4c04a
DAYORITO <107157823+DAYORITO@users.noreply.github.com>
2023-06-26 10:04:31 -0500

    Update cobros.js

--- BackAPPDANIEL/controller/cobros.js
+++ BackAPPDANIEL/controller/cobros.js
@@ -16,7 +16,7 @@ const getCobro = async (req, res) => {
 };
 const postCobro = async (req, res) => {
     try{
-        const body = req.query;
+        const body = req.body;
         const cobro = new Cobro(body);
         console.log(body);
         await cobro.save();
@@ -34,7 +34,7 @@ const postCobro = async (req, res) => {
 
 const putCobro = async (req, res) => {
     try{
-        const body = req.query;
+        const body = req.body;
         let camposEditar = {...body}
         console.log(camposEditar)
         delete camposEditar._id;
@@ -43,8 +43,8 @@ const putCobro = async (req, res) => {
         delete camposEditar.tipoCobro
         console.log(camposEditar)
         console.log(body._id)
-        const cobro = await Cobro.findOneAndUpdate({codigoCobro: body.codigoCobro}, camposEditar, {new: true});
-        const cobroModificado = await Cobro.find({codigoCobro: body.codigoCobro})
+        const cobro = await Cobro.findOneAndUpdate({_id: body._id}, camposEditar, {new: true});
+        const cobroModificado = await Cobro.find({_id: body._id})
         res.json({
             mensaje: "Se modifico exitosamente el cobro",
             cobro,
@@ -59,7 +59,7 @@ const putCobro = async (req, res) => {
     };
 };
 const deleteCobro = async (req, res) => {
-    const body = req.query;
+    const body = req.body;
     console.log(body);
     const cobro = await Cobro.findOneAndDelete({tipoCobro: body.tipoCobro});
     res.json({
